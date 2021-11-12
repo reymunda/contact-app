a8495a87de98f06624a1c0eca1bf2412c3ce382c
reymundadwi <reymunda613@gmail.com>
2021-11-12 15:03:04 +0700

    input contact with json

--- index.js
+++ index.js
@@ -8,3 +8,24 @@ if(!fs.existsSync('./data')){
 if(!fs.existsSync('./data/contacts.json')){
     fs.writeFileSync('./data/contacts.json','[]','utf-8');
 }
+
+let contacts = JSON.parse(fs.readFileSync('./data/contacts.json', 'utf-8'));
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+rl.question('Enter your name: ', (name) => {
+    rl.question('Enter your phone number: ', (phone) => {
+       let contact = {name, phone};
+       contacts.push(contact);
+
+       fs.writeFile('./data/contacts.json', JSON.stringify(contacts), (err) => {
+        if(err) throw err;
+       })
+       console.log('Data successfully entered!');
+
+       rl.close();
+    })
+})
