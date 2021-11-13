11f1d2b9847294c3b8e046b2a70167d11199997c
reymundadwi <reymunda613@gmail.com>
2021-11-13 16:11:05 +0700

    readline refactor with async await

--- index.js
+++ index.js
@@ -9,23 +9,52 @@ if(!fs.existsSync('./data/contacts.json')){
     fs.writeFileSync('./data/contacts.json','[]','utf-8');
 }
 
-let contacts = JSON.parse(fs.readFileSync('./data/contacts.json', 'utf-8'));
+// let contacts = JSON.parse(fs.readFileSync('./data/contacts.json', 'utf-8'));
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 })
 
-rl.question('Enter your name: ', (name) => {
-    rl.question('Enter your phone number: ', (phone) => {
-       let contact = {name, phone};
-       contacts.push(contact);
+// rl.question('Enter your name: ', (name) => {
+//     rl.question('Enter your phone number: ', (phone) => {
+//        let contact = {name, phone};
+//        contacts.push(contact);
 
-       fs.writeFile('./data/contacts.json', JSON.stringify(contacts), (err) => {
-        if(err) throw err;
-       })
-       console.log('Data successfully entered!');
+//        fs.writeFile('./data/contacts.json', JSON.stringify(contacts), (err) => {
+//         if(err) throw err;
+//        })
+//        console.log('Data successfully entered!');
 
-       rl.close();
+//        rl.close();
+//     })
+// })
+
+const makeQuestion = (question) => {
+    return new Promise(resolve => {
+        rl.question(question, (answer) => {
+           resolve(answer); 
+        })
     })
-})
+}
+
+const main = async () => {
+    const name = await makeQuestion("Enter your name          : "),
+          phone = await makeQuestion("Enter your mobile phone  : "),
+          email = await makeQuestion("Enter your email         : ");
+    
+    let contact = {name,phone,email};
+
+    let contacts = JSON.parse(fs.readFileSync('./data/contacts.json','utf-8'));
+    contacts.push(contact);
+
+    fs.writeFile('./data/contacts.json', JSON.stringify(contacts,null,2), (err) => {
+        if(err) throw err;
+    })
+
+    console.log('Contact berhasil ditambahkan!');
+
+    rl.close();
+}
+
+main();
